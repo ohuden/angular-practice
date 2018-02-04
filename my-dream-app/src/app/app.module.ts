d88baa6f5c14b8f6d1240e2b933799fa428513fd
ohuden <ohuden@gmail.com>
2018-02-05 01:37:38 +0200

    implemented todo logic

--- my-dream-app/src/app/app.module.ts
+++ my-dream-app/src/app/app.module.ts
@@ -4,17 +4,27 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TodoInputComponent } from './components/todo-input/todo-input.component';
+
+import { TodoService } from './services/todo.service';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TodoInputComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    TodoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
